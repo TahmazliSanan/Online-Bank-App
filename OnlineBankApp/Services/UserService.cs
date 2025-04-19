f09e8db892b4d5dbb04ecee5efffdc6f994b7cb4
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 15:11:42 +0400

    Creating UserService

---
+++ OnlineBankApp/Services/UserService.cs
@@ -0,0 +1,69 @@
+﻿using OnlineBankApp.DataAccess.DataContext;
+using OnlineBankApp.Dtos;
+using OnlineBankApp.Entities;
+
+namespace OnlineBankApp.Services
+{
+    public class UserService
+    {
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public UserDto RegisterUser(RegisterDto dto)
+        {
+            var registeredUser = _context.Users
+                .FirstOrDefault(u => u.Username.ToLower()
+                .Equals(dto.Username.ToLower()));
+
+            if (registeredUser is not null)
+                throw new Exception("User is already registered!");
+
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(dto.Password);
+
+            var newUser = new User
+            {
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Username = dto.Username,
+                HashedPassword = hashedPassword,
+            };
+
+            var card = new Card
+            {
+                Number = GenerateCardNumber(),
+                Balance = 0,
+                User = newUser
+            };
+
+            _context.Users.Add(newUser);
+            _context.Cards.Add(card);
+            _context.SaveChanges();
+
+            return new UserDto
+            {
+                Id = newUser.Id,
+                FullName = $"{newUser.FirstName} {newUser.LastName}",
+                Username = newUser.Username,
+                CardNumber = card.Number
+            };
+        }
+
+        private string GenerateCardNumber()
+        {
+            var random = new Random();
+            var cardNumber = string.Empty;
+
+            do
+            {
+                cardNumber = string.Join("-", Enumerable.Range(0, 4)
+                    .Select(_ => random.Next(1000, 9999).ToString()));
+            } while (_context.Cards.Any(c => c.Number.Equals(cardNumber)));
+
+            return cardNumber;
+        }
+    }
+}
