650a9dac0f01f48aa8f1429d9d8c5fb16033a8c0
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 11:51:53 +0400

    Creating DbContext

---
+++ OnlineBankApp/DataAccess/DataContext/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OnlineBankApp.DataAccess.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+        static string connectionString = "server=localhost;database=BankAppDb;user=root;password=23042002";
+
+        public AppDbContext(DbContextOptionsBuilder builder)
+        {
+            builder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
