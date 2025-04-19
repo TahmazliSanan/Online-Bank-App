cb8ea22ab9b40abed923f03d7eef3241bc68eac7
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 11:44:07 +0400

    Add new property

--- OnlineBankApp/Entities/User.cs
+++ OnlineBankApp/Entities/User.cs
@@ -4,6 +4,7 @@
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public string HashedPassword { get; set; } = string.Empty;
         public Card? Card { get; set; }
     }
