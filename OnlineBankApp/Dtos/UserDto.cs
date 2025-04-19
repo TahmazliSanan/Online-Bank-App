42a35f04d7845bee1d7922248db452ec6b71bb35
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 12:36:49 +0400

    Creating UserDto

---
+++ OnlineBankApp/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineBankApp.Dtos
+{
+    public class UserDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string CardNumber { get; set; } = string.Empty;
+    }
+}
