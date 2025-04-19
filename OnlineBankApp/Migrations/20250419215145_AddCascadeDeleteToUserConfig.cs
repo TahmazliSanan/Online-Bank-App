116ce827e2fe22f57679d18a9ebb69aafbec9211
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-20 01:54:09 +0400

    Adding new migration

---
+++ OnlineBankApp/Migrations/20250419215145_AddCascadeDeleteToUserConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineBankApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDeleteToUserConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
