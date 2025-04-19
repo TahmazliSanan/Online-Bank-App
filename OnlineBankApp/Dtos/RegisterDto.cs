d77f6a2526eccbf9ce3a6908ef3610eeb17172b1
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 12:36:30 +0400

    Creating RegisterDto

---
+++ OnlineBankApp/Dtos/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineBankApp.Dtos
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
