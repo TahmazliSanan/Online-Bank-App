81fec95fd82da7788b4b33f3511834c5d7a86b9a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 12:05:40 +0400

    Creating DbContextFactory

---
+++ OnlineBankApp/DataAccess/DataContext/AppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OnlineBankApp.DataAccess.DataContext
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = "server=localhost;database=BankAppDb;user=root;password=23042002";
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
