2571ce273ff3de0802ec232d191e467edea5b1b7
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 11:36:22 +0400

    Creating entities

---
+++ OnlineBankApp/Entities/Card.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineBankApp.Entities
+{
+    public class Card : BaseEntity
+    {
+        public string Number { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
---
+++ OnlineBankApp/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineBankApp.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string HashedPassword { get; set; } = string.Empty;
+        public Card? Card { get; set; }
+    }
+}
