fd5830e1487ee2cd72054f69cfbaf163ff5faddc
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-19 14:01:50 +0400

    Add new property

--- OnlineBankApp/Dtos/UserDto.cs
+++ OnlineBankApp/Dtos/UserDto.cs
@@ -2,6 +2,7 @@
 {
     public class UserDto
     {
+        public long Id { get; set; }
         public string FullName { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string CardNumber { get; set; } = string.Empty;
