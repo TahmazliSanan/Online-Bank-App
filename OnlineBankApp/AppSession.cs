fa627c0ccc49e6bec0922bac9e49d28cef3a48fa
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-20 12:34:32 +0400

    Creating AppSession

---
+++ OnlineBankApp/AppSession.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineBankApp
+{
+    public static class AppSession
+    {
+        public static long UserId { get; set; }
+        public static string FullName { get; set; } = string.Empty;
+        public static string CardNumber { get; set; } = string.Empty;
+    }
+}
