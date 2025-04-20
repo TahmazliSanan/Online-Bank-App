3a9a15ea9b1479f7e6d242ba728f547e5e1f3e66
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-20 11:19:57 +0400

    Creating CardDto

---
+++ OnlineBankApp/Dtos/CardDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineBankApp.Dtos
+{
+    public class CardDto
+    {
+        public int Id { get; set; }
+        public string OwnerFullName { get; set; } = string.Empty;
+        public string OwnerUsername { get; set; } = string.Empty;
+        public string Number { get; set; } = string.Empty;
+        public decimal Balance { get; set; }
+    }
+}
