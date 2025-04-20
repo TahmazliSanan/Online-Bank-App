653f0d1ba9dde06be9d0ec27204b5de772b98ae2
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-20 12:35:05 +0400

    Creating AddAmountForm

---
+++ OnlineBankApp/Forms/AddAmountForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace OnlineBankApp.Forms
+{
+    partial class AddAmountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddAmountForm));
+            grbAmount = new GroupBox();
+            txbAmount = new TextBox();
+            lblAddAmount = new Label();
+            btnAddAmountToCard = new Button();
+            grbAmount.SuspendLayout();
+            SuspendLayout();
+            // 
+            // grbAmount
+            // 
+            grbAmount.Controls.Add(txbAmount);
+            grbAmount.Location = new Point(12, 74);
+            grbAmount.Name = "grbAmount";
+            grbAmount.Size = new Size(246, 71);
+            grbAmount.TabIndex = 0;
+            grbAmount.TabStop = false;
+            grbAmount.Text = "Amount";
+            // 
+            // txbAmount
+            // 
+            txbAmount.Font = new Font("Segoe UI", 12F);
+            txbAmount.Location = new Point(10, 28);
+            txbAmount.Name = "txbAmount";
+            txbAmount.Size = new Size(230, 29);
+            txbAmount.TabIndex = 0;
+            // 
+            // lblAddAmount
+            // 
+            lblAddAmount.AutoSize = true;
+            lblAddAmount.Font = new Font("Segoe UI", 20F);
+            lblAddAmount.Location = new Point(41, 9);
+            lblAddAmount.Name = "lblAddAmount";
+            lblAddAmount.Size = new Size(193, 37);
+            lblAddAmount.TabIndex = 1;
+            lblAddAmount.Text = "ADD AMOUNT";
+            // 
+            // btnAddAmountToCard
+            // 
+            btnAddAmountToCard.Font = new Font("Segoe UI", 12F);
+            btnAddAmountToCard.Location = new Point(22, 160);
+            btnAddAmountToCard.Name = "btnAddAmountToCard";
+            btnAddAmountToCard.Size = new Size(230, 48);
+            btnAddAmountToCard.TabIndex = 2;
+            btnAddAmountToCard.Text = "Add Amount";
+            btnAddAmountToCard.UseVisualStyleBackColor = true;
+            btnAddAmountToCard.Click += btnAddAmountToCard_Click;
+            // 
+            // AddAmountForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(270, 217);
+            Controls.Add(btnAddAmountToCard);
+            Controls.Add(lblAddAmount);
+            Controls.Add(grbAmount);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "AddAmountForm";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "MovaBank";
+            grbAmount.ResumeLayout(false);
+            grbAmount.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox grbAmount;
+        private TextBox txbAmount;
+        private Label lblAddAmount;
+        private Button btnAddAmountToCard;
+    }
+}
---
+++ OnlineBankApp/Forms/AddAmountForm.cs
@@ -0,0 +1,41 @@
+﻿using OnlineBankApp.Services;
+
+namespace OnlineBankApp.Forms
+{
+    public partial class AddAmountForm : Form
+    {
+        private readonly CardService _cardService;
+
+        public AddAmountForm(UserService userService, CardService cardService)
+        {
+            InitializeComponent();
+            _cardService = cardService;
+        }
+
+        private void btnAddAmountToCard_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var amount = txbAmount.Text;
+
+                if (string.IsNullOrWhiteSpace(amount))
+                    MessageBox.Show("Field cannot be empty!", "Warning", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                if (decimal.TryParse(amount, out decimal amountAsDecimal))
+                {
+                    var cardDto = _cardService.AddAmountToCard(AppSession.CardNumber, amountAsDecimal);
+                    MessageBox.Show("Amount added successfully!", "Information", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                Hide();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
