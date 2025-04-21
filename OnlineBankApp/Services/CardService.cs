38f71e17d76290b01e631eb15feb9f37ec6e6a90
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-21 12:55:49 +0400

    Add new method: WithdrawAmountFromCard

--- OnlineBankApp/Services/CardService.cs
+++ OnlineBankApp/Services/CardService.cs
@@ -13,6 +13,32 @@ namespace OnlineBankApp.Services
             _context = context;
         }
 
+        public CardDto WithdrawAmountFromCard(string cardNumber, decimal amount)
+        {
+            var card = _context.Cards
+                .Include(c => c.User)
+                .FirstOrDefault(c => c.Number.ToLower()
+                .Equals(cardNumber.ToLower())) ?? throw new Exception("Card number is not valid!");
+
+            if (amount <= 0)
+                throw new Exception("Amount cannot be less than or equal to zero!");
+
+            if (card.Balance < amount)
+                throw new Exception("Insufficient balance!");
+
+            card.Balance -= amount;
+            _context.SaveChanges();
+
+            return new CardDto
+            {
+                Id = card.Id,
+                OwnerFullName = $"{card.User!.FirstName} {card.User!.LastName}",
+                OwnerUsername = card.User!.Username,
+                Number = cardNumber,
+                Balance = card.Balance,
+            };
+        }
+
         public CardDto AddAmountToCard(string cardNumber, decimal amount)
         {
             var card = _context.Cards
