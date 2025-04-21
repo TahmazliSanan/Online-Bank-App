eb0aca585d00454c0b47f0fdb4baf1243af8dfde
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-21 23:53:18 +0400

    Add new property

--- OnlineBankApp/AppSession.cs
+++ OnlineBankApp/AppSession.cs
@@ -1,9 +1,12 @@
-﻿namespace OnlineBankApp
+﻿using OnlineBankApp.Dtos;
+
+namespace OnlineBankApp
 {
     public static class AppSession
     {
         public static long UserId { get; set; }
         public static string FullName { get; set; } = string.Empty;
         public static string CardNumber { get; set; } = string.Empty;
+        public static UserDto? UserDto { get; set; }
     }
 }
