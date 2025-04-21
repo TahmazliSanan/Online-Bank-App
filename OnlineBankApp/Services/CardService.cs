c4588c5b95fd270f9afe5c808c398d2c0a9b563b
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-21 13:50:05 +0400

    Add new method: TransferAmountBetweenTwoCards

--- OnlineBankApp/Services/CardService.cs
+++ OnlineBankApp/Services/CardService.cs
@@ -13,6 +13,42 @@ namespace OnlineBankApp.Services
             _context = context;
         }
 
+        public CardDto TransferAmountBetweenTwoCards(string senderCardNumber,
+            string receiverCardNumber, decimal amount)
+        {
+            if (senderCardNumber.ToLower().Equals(receiverCardNumber.ToLower()))
+                throw new Exception("Cannot transfer to the same card!");
+
+            var senderCard = _context.Cards
+                .Include(c => c.User)
+                .FirstOrDefault(c => c.Number.ToLower()
+                .Equals(senderCardNumber.ToLower())) ?? throw new Exception("Sender card number is not valid!");
+
+            var receiverCard = _context.Cards
+                .Include(c => c.User)
+                .FirstOrDefault(c => c.Number.ToLower()
+                .Equals(receiverCardNumber.ToLower())) ?? throw new Exception("Receiver card number is not valid!");
+
+            if (amount <= 0)
+                throw new Exception("Amount cannot be less than or equal to zero!");
+
+            if (senderCard.Balance < amount)
+                throw new Exception("Insufficient balance!");
+
+            senderCard.Balance -= amount;
+            receiverCard.Balance += amount;
+            _context.SaveChanges();
+
+            return new CardDto
+            {
+                Id = senderCard.Id,
+                OwnerFullName = $"{senderCard.User!.FirstName} {senderCard.User!.LastName}",
+                OwnerUsername = senderCard.User!.Username,
+                Number = senderCardNumber,
+                Balance = senderCard.Balance
+            };
+        }
+
         public CardDto WithdrawAmountFromCard(string cardNumber, decimal amount)
         {
             var card = _context.Cards
@@ -35,7 +71,7 @@ namespace OnlineBankApp.Services
                 OwnerFullName = $"{card.User!.FirstName} {card.User!.LastName}",
                 OwnerUsername = card.User!.Username,
                 Number = cardNumber,
-                Balance = card.Balance,
+                Balance = card.Balance
             };
         }
 
@@ -58,7 +94,7 @@ namespace OnlineBankApp.Services
                 OwnerFullName = $"{card.User!.FirstName} {card.User!.LastName}",
                 OwnerUsername = card.User!.Username,
                 Number = cardNumber,
-                Balance = card.Balance,
+                Balance = card.Balance
             };
         }
     }
