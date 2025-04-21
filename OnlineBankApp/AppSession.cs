4f18a07560b084e8db4aa746a3941dae789fc09b
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-22 00:15:45 +0400

    Removing unused properties

--- OnlineBankApp/AppSession.cs
+++ OnlineBankApp/AppSession.cs
@@ -4,9 +4,6 @@ namespace OnlineBankApp
 {
     public static class AppSession
     {
-        public static long UserId { get; set; }
-        public static string FullName { get; set; } = string.Empty;
-        public static string CardNumber { get; set; } = string.Empty;
-        public static UserDto? UserDto { get; set; }
+        public static UserDto? LoggedInUser { get; set; }
     }
 }
