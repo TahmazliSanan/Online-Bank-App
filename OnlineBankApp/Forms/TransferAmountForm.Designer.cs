9725b2f31c56c8b8062f4315931b6ccbd6daa6c6
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-21 13:48:32 +0400

    Creating TransferAmountForm

---
+++ OnlineBankApp/Forms/TransferAmountForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace OnlineBankApp.Forms
+{
+    partial class TransferAmountForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransferAmountForm));
+            btnTransferAmountBetweenTwoCards = new Button();
+            lblAddAmount = new Label();
+            grbAmount = new GroupBox();
+            txbAmount = new TextBox();
+            grbCardNumber = new GroupBox();
+            txbCardNumber = new TextBox();
+            grbAmount.SuspendLayout();
+            grbCardNumber.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnTransferAmountBetweenTwoCards
+            // 
+            btnTransferAmountBetweenTwoCards.Font = new Font("Segoe UI", 12F);
+            btnTransferAmountBetweenTwoCards.Location = new Point(22, 241);
+            btnTransferAmountBetweenTwoCards.Name = "btnTransferAmountBetweenTwoCards";
+            btnTransferAmountBetweenTwoCards.Size = new Size(230, 48);
+            btnTransferAmountBetweenTwoCards.TabIndex = 3;
+            btnTransferAmountBetweenTwoCards.Text = "Transfer Amount";
+            btnTransferAmountBetweenTwoCards.UseVisualStyleBackColor = true;
+            btnTransferAmountBetweenTwoCards.Click += btnTransferAmountBetweenTwoCards_Click;
+            // 
+            // lblAddAmount
+            // 
+            lblAddAmount.AutoSize = true;
+            lblAddAmount.Font = new Font("Segoe UI", 15F);
+            lblAddAmount.Location = new Point(37, 9);
+            lblAddAmount.Name = "lblAddAmount";
+            lblAddAmount.Size = new Size(195, 28);
+            lblAddAmount.TabIndex = 4;
+            lblAddAmount.Text = "TRANSFER AMOUNT";
+            // 
+            // grbAmount
+            // 
+            grbAmount.Controls.Add(txbAmount);
+            grbAmount.Location = new Point(12, 154);
+            grbAmount.Name = "grbAmount";
+            grbAmount.Size = new Size(246, 71);
+            grbAmount.TabIndex = 2;
+            grbAmount.TabStop = false;
+            grbAmount.Text = "Amount";
+            // 
+            // txbAmount
+            // 
+            txbAmount.Font = new Font("Segoe UI", 12F);
+            txbAmount.Location = new Point(10, 28);
+            txbAmount.Name = "txbAmount";
+            txbAmount.Size = new Size(230, 29);
+            txbAmount.TabIndex = 2;
+            // 
+            // grbCardNumber
+            // 
+            grbCardNumber.Controls.Add(txbCardNumber);
+            grbCardNumber.Location = new Point(12, 77);
+            grbCardNumber.Name = "grbCardNumber";
+            grbCardNumber.Size = new Size(246, 71);
+            grbCardNumber.TabIndex = 1;
+            grbCardNumber.TabStop = false;
+            grbCardNumber.Text = "Card Number";
+            // 
+            // txbCardNumber
+            // 
+            txbCardNumber.Font = new Font("Segoe UI", 12F);
+            txbCardNumber.Location = new Point(10, 28);
+            txbCardNumber.Name = "txbCardNumber";
+            txbCardNumber.Size = new Size(230, 29);
+            txbCardNumber.TabIndex = 1;
+            // 
+            // TransferAmountForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(270, 298);
+            Controls.Add(grbCardNumber);
+            Controls.Add(btnTransferAmountBetweenTwoCards);
+            Controls.Add(lblAddAmount);
+            Controls.Add(grbAmount);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "TransferAmountForm";
+            Text = "MovaBank";
+            grbAmount.ResumeLayout(false);
+            grbAmount.PerformLayout();
+            grbCardNumber.ResumeLayout(false);
+            grbCardNumber.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnTransferAmountBetweenTwoCards;
+        private Label lblAddAmount;
+        private GroupBox grbAmount;
+        private TextBox txbAmount;
+        private GroupBox grbCardNumber;
+        private TextBox txbCardNumber;
+    }
+}
---
+++ OnlineBankApp/Forms/TransferAmountForm.cs
@@ -0,0 +1,57 @@
+﻿using OnlineBankApp.Services;
+
+namespace OnlineBankApp.Forms
+{
+    public partial class TransferAmountForm : Form
+    {
+        private readonly UserService _userService;
+        private readonly CardService _cardService;
+
+        public TransferAmountForm(UserService userService, CardService cardService)
+        {
+            InitializeComponent();
+            _userService = userService;
+            _cardService = cardService;
+        }
+
+        private void btnTransferAmountBetweenTwoCards_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var receiverCardNumber = txbCardNumber.Text;
+                var amount = txbAmount.Text;
+
+                if (string.IsNullOrWhiteSpace(receiverCardNumber) 
+                    || string.IsNullOrWhiteSpace(amount))
+                {
+                    MessageBox.Show("Fields cannot be empty!", "Warning",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (decimal.TryParse(amount, out decimal amountAsDecimal))
+                {
+                    var cardDto = _cardService.TransferAmountBetweenTwoCards(AppSession.CardNumber, receiverCardNumber, amountAsDecimal);
+
+                    MessageBox.Show("Amount was transferred successfully! Please login again!", 
+                        "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid amount format! Please try again!", 
+                        "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                Hide();
+                var loginForm = new LoginForm(_userService, _cardService);
+                loginForm.Show();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
