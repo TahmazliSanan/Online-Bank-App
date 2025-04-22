313e529d14618ff380582aaba1cf65af4986fe2d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-22 23:42:51 +0400

    Creating Transaction entity

---
+++ OnlineBankApp/Entities/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using OnlineBankApp.Enums;
+
+namespace OnlineBankApp.Entities
+{
+    public class Transaction : BaseEntity
+    {
+        public string SenderCardNumber { get; set; } = string.Empty;
+        public string? ReceiverCardNumber { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime ProcessedDate { get; set; }
+        public int UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
