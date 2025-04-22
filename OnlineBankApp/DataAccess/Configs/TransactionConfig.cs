5299f0bc3f4bee0294b582eb28e6c3fea1874b89
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-22 23:55:49 +0400

    Create TransactionConfig

---
+++ OnlineBankApp/DataAccess/Configs/TransactionConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineBankApp.Entities;
+
+namespace OnlineBankApp.DataAccess.Configs
+{
+    public class TransactionConfig : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Type)
+           .HasConversion<string>()
+           .IsRequired();
+
+            builder.HasOne(t => t.User)
+            .WithMany(u => u.Transactions)
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            builder.ToTable("transactions");
+        }
+    }
+}
