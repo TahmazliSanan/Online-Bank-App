9301abdea9c5820108002cd4d357fea095130a96
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-04-23 10:44:06 +0400

    Creating ReportDto

---
+++ OnlineBankApp/Dtos/ReportDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineBankApp.Dtos
+{
+    public class ReportDto
+    {
+        public long Id { get; set; }
+        public string OwnerFullName { get; set; } = string.Empty;
+        public string SenderCardNumber { get; set; } = string.Empty;
+        public string? ReceiverCardNumber { get; set; }
+        public string TransactionType { get; set; } = string.Empty;
+        public DateTime ProcessedDate { get; set; }
+    }
+}
